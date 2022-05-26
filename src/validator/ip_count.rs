ec23358f43a38ff0e31083976702b138b620d517
hehegrrrhehegrr <ilwhmcwo@tuta.io>
2022-05-26 16:34:21 +0300

    Update due to new requirements

--- src/validator/ip_count.rs
+++ src/validator/ip_count.rs
@@ -1,9 +1,11 @@
-use crate::model::{BanRequest, BanTarget, Request};
-use crate::validator::Validator;
+use std::collections::HashMap;
+
 use anyhow::Error;
 use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+
+use crate::model::{BanRequest, BanTarget, Request};
+use crate::validator::Validator;
 
 #[derive(Clone, Copy, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
@@ -31,17 +33,21 @@ impl From<BanRuleConfig> for BanRule {
 
 #[derive(Debug)]
 pub struct Data {
-    count: u64,
-    last: DateTime<Utc>,
-    banned_till: DateTime<Utc>,
+    requests_since_last_ban: u64,
+    applied_rule_id: Option<usize>,
+    recent_requests: Vec<DateTime<Utc>>,
+    banned_since: DateTime<Utc>,
+    resets_at: DateTime<Utc>,
 }
 
 impl Default for Data {
     fn default() -> Self {
         Data {
-            count: 0,
-            last: Utc::now(),
-            banned_till: DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(0, 0), Utc),
+            requests_since_last_ban: 0,
+            applied_rule_id: None,
+            recent_requests: vec![],
+            banned_since: DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(0, 0), Utc),
+            resets_at: DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(0, 0), Utc),
         }
     }
 }
@@ -61,42 +67,67 @@ impl IPCount {
             ip_data,
         }
     }
+
+    fn ban_by_ip(&self, rule_idx: usize, ip: String) -> BanRequest {
+        BanRequest {
+            target: BanTarget {
+                ip: Some(ip),
+                user_agent: None,
+            },
+            reason: self.ban_desc.clone(),
+            ttl: self.rules.get(rule_idx).expect("rule not found").ban_duration.num_seconds() as u32,
+            analyzer: self.name(),
+        }
+    }
 }
 
 impl Validator for IPCount {
     fn validate(&mut self, req: Request) -> Result<Option<BanRequest>, Error> {
         let ip = req.remote_ip;
         let mut data = self.ip_data.entry(ip.clone()).or_insert(Data::default());
-        data.count += 1;
 
-        let rule_idx = self.rules.binary_search_by(|r| r.limit.cmp(&data.count));
+        let now = Utc::now();
+        data.recent_requests.push(now.clone());
 
-        let rule = match rule_idx {
-            Ok(idx) => self.rules.get(idx), // Rule for exact count
-            Err(_) => return Ok(None), // Ignore any other case
+        // No ban now
+        if data.applied_rule_id.is_none() {
+            let rule = self.rules.get(0).expect("at least one rule required");
+            data.recent_requests.retain(|r| *r > now - rule.reset_duration);
+            if data.recent_requests.len() < rule.limit as usize { // limits are ok
+                return Ok(None);
+            }
+            data.resets_at = Utc::now() + rule.reset_duration;
+            data.recent_requests.clear();
+            data.requests_since_last_ban = 0;
+            data.applied_rule_id = Some(0);
+
+            return Ok(Some(self.ban_by_ip(0, ip)));
         }
-        .expect("no rules set");
 
-        // reset duration exceeds from last request
-        if data.last + rule.ban_duration <= Utc::now() {
-            data.count = 1
-        }
-        data.last = Utc::now();
+        //  was banned
 
-        if rule.limit <= data.count {
-            data.banned_till = data.last + rule.ban_duration;
-            Ok(Some(BanRequest {
-                target: BanTarget {
-                    ip: Some(ip.clone()),
-                    user_agent: None,
-                },
-                reason: self.ban_desc.clone(),
-                ttl: rule.ban_duration.num_seconds() as u32,
-                analyzer: self.name(),
-            }))
-        } else {
-            Ok(None)
+        // if that ban should be reset
+        if data.resets_at <= Utc::now() && data.applied_rule_id.is_some() {
+            data.applied_rule_id = None;
+            return Ok(None);
         }
+
+        data.requests_since_last_ban += 1;
+
+        // current limit; possibly was banned with previous rule
+        let rule_idx = data.applied_rule_id.map_or(0, |v| v + 1);
+        let rule = self.rules.get(rule_idx).expect("rule not found");
+
+        if data.requests_since_last_ban >= rule.limit {
+            data.banned_since = Utc::now();
+            data.resets_at = data.banned_since + rule.reset_duration;
+            data.requests_since_last_ban = 0;
+            data.applied_rule_id = Some(rule_idx + 1);
+
+            return Ok(Some(self.ban_by_ip(rule_idx, ip)));
+        }
+
+        Ok(None)
     }
 
     fn name(&self) -> String {
@@ -106,42 +137,37 @@ impl Validator for IPCount {
 
 #[cfg(test)]
 mod tests {
-    use crate::model::{BanRequest, BanTarget, Request};
-    use crate::validator::ip_count::{BanRule, IPCount};
-    use crate::validator::Validator;
     use anyhow::Error;
     use chrono::Duration;
 
-    fn print_ip_data(v: &IPCount) {
-        for (ip, data) in &v.ip_data {
-            println!("{}: {:?}", ip, data)
-        }
-    }
+    use crate::model::{BanRequest, BanTarget, Request};
+    use crate::validator::ip_count::{BanRule, IPCount};
+    use crate::validator::Validator;
 
     /// get_default_validator returns IPCount with
     /// next limits:
     ///
-    /// 2 -> 1s ban, 2s reset
+    /// 3 -> 1s ban, 2s reset
     ///
-    /// 5 -> 3s ban, 6s reset
+    /// 2 -> 3s ban, 6s reset
     ///
-    /// 10 -> 4s ban, 8s reset
+    /// 1 -> 4s ban, 8s reset
     fn get_default_validator() -> IPCount {
         IPCount {
             ban_desc: "".to_string(),
             rules: vec![
                 BanRule {
-                    limit: 2,
+                    limit: 3,
                     ban_duration: Duration::seconds(1),
                     reset_duration: Duration::seconds(2),
                 },
                 BanRule {
-                    limit: 5,
+                    limit: 2,
                     ban_duration: Duration::seconds(3),
                     reset_duration: Duration::seconds(6),
                 },
                 BanRule {
-                    limit: 10,
+                    limit: 1,
                     ban_duration: Duration::seconds(4),
                     reset_duration: Duration::seconds(8),
                 },
@@ -151,7 +177,8 @@ mod tests {
     }
 
     pub struct TestCase {
-        pub input: Vec<(Request, Duration)>, //request, sleep before request
+        pub input: Vec<(Request, Duration)>,
+        //request, sleep before request
         pub want_last: Option<Result<Option<BanRequest>, Error>>,
         pub want_every: Option<Vec<Option<BanRequest>>>,
     }
@@ -171,6 +198,7 @@ mod tests {
     fn exceed_requests_leads_to_ban() {
         let tc = TestCase {
             input: vec![
+                (req_with_ip("1.1.1.1"), Duration::seconds(0)),
                 (req_with_ip("1.1.1.1"), Duration::seconds(0)),
                 (req_with_ip("1.1.1.1"), Duration::seconds(0)),
             ],
@@ -237,11 +265,44 @@ mod tests {
         let tc = TestCase {
             input: vec![
                 (req_with_ip("1.1.1.1"), Duration::seconds(0)),
-                (req_with_ip("1.1.1.1"), Duration::seconds(2)),
+                (req_with_ip("1.1.1.1"), Duration::seconds(0)),
                 (req_with_ip("1.1.1.1"), Duration::seconds(2)),
             ],
             want_last: Some(Ok(None)),
             want_every: Some(vec![None, None, None]),
+        };
+
+        let mut results = vec![];
+        let mut v = get_default_validator();
+        for (req, dur) in tc.input {
+            std::thread::sleep(dur.to_std().unwrap());
+            if let Ok(r) = v.validate(req) {
+                results.push(r);
+            }
+        }
+
+        assert!(tc.want_every.is_some() || tc.want_last.is_some());
+        if let Some(ev) = tc.want_every {
+            assert_eq!(ev, results)
+        }
+        if let Some(Ok(ev)) = tc.want_last {
+            let res = results.pop().unwrap();
+            assert_eq!(ev, res)
+        }
+    }
+
+    #[test]
+    fn rate_limit_doesnt_lead_to_ban() {
+        let tc = TestCase {
+            input: vec![
+                (req_with_ip("1.1.1.1"), Duration::seconds(0)),
+                (req_with_ip("1.1.1.1"), Duration::seconds(1)),
+                (req_with_ip("1.1.1.1"), Duration::seconds(1)),
+                (req_with_ip("1.1.1.1"), Duration::seconds(1)),
+                (req_with_ip("1.1.1.1"), Duration::seconds(1)),
+            ],
+            want_last: None,
+            want_every: Some(vec![None, None, None, None, None]),
         };
 
         let mut results = vec![];
@@ -270,9 +331,11 @@ mod tests {
                 (req_with_ip("1.1.1.1"), Duration::seconds(0)),
                 (req_with_ip("1.1.1.1"), Duration::seconds(0)),
                 (req_with_ip("1.1.1.1"), Duration::seconds(0)),
+                (req_with_ip("1.1.1.1"), Duration::seconds(0)),
             ],
             want_last: None,
             want_every: Some(vec![
+                None,
                 None,
                 Some(BanRequest {
                     target: BanTarget {
@@ -319,6 +382,7 @@ mod tests {
             want_last: None,
             want_every: Some(vec![
                 None,
+                None,
                 Some(BanRequest {
                     target: BanTarget {
                         ip: Some("1.1.1.1".to_string()),
@@ -328,7 +392,6 @@ mod tests {
                     ttl: 1,
                     analyzer: "ip_count".to_string(),
                 }),
-                None,
                 None,
                 Some(BanRequest {
                     target: BanTarget {
@@ -361,6 +424,7 @@ mod tests {
             assert_eq!(ev, res)
         }
     }
+
     #[test]
     fn one_ip_provides_ban_only_for_itself() {
         let tc = TestCase {
@@ -369,10 +433,13 @@ mod tests {
                 (req_with_ip("2.2.2.2"), Duration::seconds(0)),
                 (req_with_ip("3.3.3.3"), Duration::seconds(0)),
                 (req_with_ip("3.3.3.3"), Duration::seconds(0)),
+                (req_with_ip("3.3.3.3"), Duration::seconds(0)),
+                (req_with_ip("1.1.1.1"), Duration::seconds(0)),
                 (req_with_ip("1.1.1.1"), Duration::seconds(0)),
             ],
             want_last: None,
             want_every: Some(vec![
+                None,
                 None,
                 None,
                 None,
@@ -385,6 +452,7 @@ mod tests {
                     ttl: 1,
                     analyzer: "ip_count".to_string(),
                 }),
+                None,
                 Some(BanRequest {
                     target: BanTarget {
                         ip: Some("1.1.1.1".to_string()),
@@ -418,21 +486,20 @@ mod tests {
     }
 
     #[test]
-    fn reset_one_ip_doesnt_resets_other_ip() {
+    fn after_reset_baned_by_first_rule() {
         let tc = TestCase {
             input: vec![
                 (req_with_ip("1.1.1.1"), Duration::seconds(0)), // None
-                (req_with_ip("1.1.1.1"), Duration::seconds(0)), // still banned for 1s, 2s resets
-                (req_with_ip("1.1.1.1"), Duration::seconds(0)), // still banned for 1s, 2s resets
-                (req_with_ip("1.1.1.1"), Duration::seconds(0)), // still banned for 1s, 2s resets
-                (req_with_ip("1.1.1.1"), Duration::seconds(0)), // banned for 3s, 6s reset
-                (req_with_ip("2.2.2.2"), Duration::seconds(0)), // None
-                (req_with_ip("2.2.2.2"), Duration::seconds(0)), // banned for 1s, 2s reset
-                (req_with_ip("2.2.2.2"), Duration::seconds(2)), // currently unbanned
+                (req_with_ip("1.1.1.1"), Duration::seconds(0)), // None
+                (req_with_ip("1.1.1.1"), Duration::seconds(0)), // banned for 1s, 2s reset
+                (req_with_ip("1.1.1.1"), Duration::seconds(2)), // currently unbanned
+                (req_with_ip("1.1.1.1"), Duration::seconds(0)), // None
+                (req_with_ip("1.1.1.1"), Duration::seconds(0)), // banned for 1s, 2s reset
             ],
             want_last: None,
             want_every: Some(vec![
                 None,
+                None,
                 Some(BanRequest {
                     target: BanTarget {
                         ip: Some("1.1.1.1".to_string()),
@@ -450,20 +517,9 @@ mod tests {
                         user_agent: None,
                     },
                     reason: "".to_string(),
-                    ttl: 3,
-                    analyzer: "ip_count".to_string(),
-                }),
-                None,
-                Some(BanRequest {
-                    target: BanTarget {
-                        ip: Some("2.2.2.2".to_string()),
-                        user_agent: None,
-                    },
-                    reason: "".to_string(),
                     ttl: 1,
                     analyzer: "ip_count".to_string(),
                 }),
-                None,
             ]),
         };
 
